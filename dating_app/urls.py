fc29f2594e2a716e759a7a446d8e709a7ca49f0b
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2025-02-13 14:29:15 +0100

    feat: implement template, view and urls

--- dating_app/urls.py
+++ dating_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # path for home page
+	path('questionnaire/', include('questionnaire.urls')),  # path for questionnaire page
 ]
