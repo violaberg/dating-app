5db08ea61f218d0b3aafaaf6f5f9aeaa838a985a
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2025-02-13 21:13:22 +0000

    setup user_messages app and necessary files

--- dating_app/urls.py
+++ dating_app/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),  # path for home page
 	path('questionnaire/', include('questionnaire.urls')),  # path for questionnaire page
+    path('messages/', include('user_messages.urls')),  # path for messages page
 ]
---
+++ user_messages/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class MessageRequest(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10,
+        choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('declined', 'Declined')],
+        default='pending'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
