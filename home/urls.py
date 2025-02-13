3f8c430f1ea7a910e13b1df6dd347442a97a2226
Viola Bergere <viola.bergere@gmail.com>
2025-02-13 20:15:36 +0000

    feat: add team page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'), # home page url
+    path('', views.home, name='home'),
+    path('team/', views.team, name='team'),
 ]
