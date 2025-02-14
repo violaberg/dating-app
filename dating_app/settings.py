83139430817d0196bc02b55c8da1c753eede0d08
Viola Bergere <viola.bergere@gmail.com>
2025-02-14 23:32:54 +0000

    chore: delete unneeded settings

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.contrib.messages import constants as messages
 import dj_database_url
 from dotenv import load_dotenv
 
@@ -75,16 +74,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
-
-MESSAGE_TAGS = {
-    messages.DEBUG: 'debug',
-    messages.INFO: 'info',
-    messages.SUCCESS: 'success',
-    messages.WARNING: 'warning',
-    messages.ERROR: 'danger',
-}
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 ROOT_URLCONF = 'dating_app.urls'
 
