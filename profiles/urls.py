33c8140d868a8917455ddf686803dc309f1abdf2
Georgeburn94 <georgeburn94@gmail.com>
2025-02-14 13:35:02 +0000

    add: user delete account button

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.view_or_edit_profile, name='profile'),
+    path('delete-profile/', views.delete_profile, name='delete_profile'),
 ]
