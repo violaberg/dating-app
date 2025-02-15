b20225c8005237e8cc1e70c3bb2d6f13f12dec74
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2025-02-15 08:02:23 +0000

    chore: add condition for development mode

--- dating_app/urls.py
+++ dating_app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from . views import error_403, error_404, error_500
@@ -33,3 +35,6 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),  # path for profiles page
     path('contact/', include('contact.urls')),
 ]
+
+if settings.DEBUG:  # Only serve media files in development mode
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
