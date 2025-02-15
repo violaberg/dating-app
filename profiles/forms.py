0a99980517967451291793e4a5432789143fb7c2
farha <farha.islam@students.codeinstitute.net>
2025-02-15 20:54:48 +0100

    Fix: CI linter error

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Profile
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_image', 'bio', 'gender', 'age', 'location', 'interests', 'relationship_goal']
+        fields = [
+            'profile_image', 'bio', 'gender', 'age', 
+            'location', 'interests', 'relationship_goal'
+        ]
         widgets = {
             'bio': forms.Textarea(attrs={'rows': 3}),
             'interests': forms.Textarea(attrs={'rows': 3}),
         }
+
+
+
