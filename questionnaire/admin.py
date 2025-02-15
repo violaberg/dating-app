51e7c63b1808ae2071b636b741721eb3fc27848a
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2025-02-15 06:34:10 +0100

    feat: enhance Django Admin for Questionnaire Management

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -3,6 +3,27 @@ from .models import QuestionCategory, Question, Answer
 
 # Register your models here.
 
-admin.site.register(QuestionCategory)
-admin.site.register(Question)
-admin.site.register(Answer)
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+@admin.register(QuestionCategory)
+class QuestionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'category', 'question_type')
+    list_filter = ('category', 'question_type')
+    search_fields = ('question_text',)
+    inlines = [AnswerInline]
+    fieldsets = (
+        (None, {'fields': ('question_text', 'category', 'question_type')}),
+    )
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer_text', 'question')
+    list_filter = ('question',)
+    search_fields = ('answer_text', 'question__question_text')
