1b48afbd7e9cbad352b5edc42a64e8084d61a0cd
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-02-17 00:20:27 +0100

    Add debugging print statements.

--- dating_app/asgi.py
+++ dating_app/asgi.py
@@ -8,12 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import sys
+from django.conf import settings
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
+
+if 'daphne' in sys.argv[0].lower():
+    print("Daphne is running.")
+else:
+    print("Daphne is NOT running, possibly using gunicorn or another server.")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dating_app.settings')
 
