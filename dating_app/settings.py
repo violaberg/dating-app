8f8e2cd6ac2c7cd437e0cc7c5eb3ddf796a79da2
Viola Bergere <viola.bergere@gmail.com>
2025-02-16 07:50:40 +0000

    style: center items on mobile navbar, center content in footer on devices under 767px

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -39,8 +39,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
-    'channels',
+    #'daphne',
+    #'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
