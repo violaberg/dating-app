68bc41403f24477816321002c8e304e6c9eaa784
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-02-17 10:01:13 +0100

    Add ChatRoom model to chat app.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    descption = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
