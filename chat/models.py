29dcbd044b505e0c0c86bc297c8d0c2a0d81e56a
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-02-17 11:13:55 +0100

    Add friendly name to ChatRoom model.

--- chat/models.py
+++ chat/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    descption = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.friendly_name or self.name
     
