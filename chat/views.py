cb67b078c7384ade9d98ee03258924236dfe5b3d
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-02-23 13:26:55 +0100

    Create custom 404 page for non-existent chatrooms.

--- chat/views.py
+++ chat/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from .models import ChatRoom
 
 
-from django.shortcuts import render
-
 def chat(request):
     chatrooms = ChatRoom.objects.all()  # Get all chatrooms
     return render(request, "chat/chat.html", {'chatrooms': chatrooms})
 
 
 def chatroom(request, room_name):
-    chatroom_exists = ChatRoom.objects.filter(name=room_name).exists()
-    
+    available_rooms = ChatRoom.objects.values_list('name', flat=True)
+    friendly_name = ChatRoom.friendly_name
+
+    chatroom_exists = room_name in available_rooms
+
     if not chatroom_exists:
         return render(request, "chat/chatroom_not_found.html", {"room_name": room_name})
 
-    return render(request, "chat/chatroom.html", {"room_name": room_name})
+    return render(request, "chat/chatroom.html", {"room_name": room_name}, {"friendly_name": friendly_name})
+    
