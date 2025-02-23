8bf7a8d5dfbb4f1c933c242989024a2657ef4a7f
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-02-23 14:06:44 +0100

    Tell users where they are in chat.

--- chat/views.py
+++ chat/views.py
@@ -9,12 +9,17 @@ def chat(request):
 
 def chatroom(request, room_name):
     available_rooms = ChatRoom.objects.values_list('name', flat=True)
-    friendly_name = ChatRoom.friendly_name
+    chatroom = ChatRoom.objects.filter(name=room_name).first()
+    friendly_name = chatroom.friendly_name
 
     chatroom_exists = room_name in available_rooms
 
     if not chatroom_exists:
         return render(request, "chat/chatroom_not_found.html", {"room_name": room_name})
-
-    return render(request, "chat/chatroom.html", {"room_name": room_name}, {"friendly_name": friendly_name})
+    
+    print(f"DEBUG: Rendering chatroom.html with room_name={room_name} and friendly_name={friendly_name}")
+    return render(request, "chat/chatroom.html", {
+        "room_name": room_name,
+        "friendly_name": friendly_name,
+    })
     
